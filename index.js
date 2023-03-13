5a3b52566ae2a3dfe53d1a2e18ea3cf468259796
Tomno <tomnoviv@gmail.com>
2023-03-13 11:41:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+
+const companyNamee="Scuber";
+
+let mostProfitableNeighborhoodd= "Chelsea";
+const mostProfitableNeighborhood= "Chelsea";
+
+let companyCeo='Susan Smith';
